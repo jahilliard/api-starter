21b1b829d8f7e5405411542e03f5235feb2159dd
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-03-29 14:36:01 -0700

    fixing tsc husky

--- packages/lib/utils.ts
+++ packages/lib/utils.ts
@@ -1,15 +1,15 @@
-import _ from "lodash"
+import _ from 'lodash'
 
-export function encodeURLQuery(query: { [s: string]: string | number | boolean }): string{
+export function encodeURLQuery(query: { [s: string]: string | number | boolean }): string {
   if (!_.isObjectLike(query)) {
-    throw new Error("Query Object Params not object ")
+    throw new Error('Query Object Params not object ')
   }
   const bodyElements = Object.entries(query) as [string, string | number | boolean][]
   return bodyElements.reduce((accumFormValue, [key, value], indx) => {
     let formVal = `${accumFormValue}${encodeURIComponent(key)}=${encodeURIComponent(value)}`
     if (indx !== bodyElements.length) {
-      formVal = formVal.concat("&")
+      formVal = formVal.concat('&')
     }
     return formVal
-  }, "")
+  }, '')
 }
